7a87f040ce88c01e6ae22545e3859f19e1aa3b0e
William GOURLEY <wgourley@e4r6p4.wethinkcode.co.za>
2018-10-06 15:16:51 +0200

    things

--- templates/landing.php
+++ templates/landing.php
@@ -31,20 +31,7 @@ require_once("init.php");
 		
 		?>
 	</header>
-		<form align="center" class="auth_form" method="post" action="admin/auth.php" id="auth">
-			<input type='hidden' name="QUERY" value="login">
-			<input name="UID" type="text" placeholder="Username" required /><br><br>
-			<input name="PASS" type="password" placeholder="password" required /><br><br>
-			<button
-				class="g-recaptcha"
-				data-sitekey="6LcYd3MUAAAAAEXNkgBpwbjKHDf33VE9eS2NICbn"
-				data-callback="on_submit">
-				login
-			</button>
-			<p style="color: white">Register: <a href="register.php" style="color: white"> Click here</a></p>
-			<p style="color: white">Forgot your password?<a href="#" style="color: white"> Click here</a></p>
-		</form>
-		<hr style="color:aliceblue">
+		
 	<footer>
 		<ul>
 			<li><a href="#">Home</a></li>
