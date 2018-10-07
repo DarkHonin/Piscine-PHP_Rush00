1fa9c23b4bdbec2b852445dfe45fa800d47af046
DGmoN <darkhonin@gmail.com>
2018-10-07 18:24:36 +0200

    semicolon

--- checkout.php
+++ checkout.php
@@ -1,13 +1,14 @@
 <?php
 
-include "init.php";
+include_once("init.php");
 if (!isset($_SESSION['UNAME']) || empty($_SESSION['UNAME']))
 	header("Location: /login.php?redirect=".$_SERVER['REQUEST_URI']);
+include "head.php";
 $_SESSION["CART"] = [];
 ?>
-<div class="product" id="p<?php echo $p["id"] ?>">
+<div class="basket_info" id="p<?php echo $p["id"] ?>">
 	<div class="product-info">
-		<h2>Empty?</h2>
+		<h2>Sucsess!</h2>
 		<div class="product-description">Thank you for your purchace.</div>
 		<a class="cart-add" href="/">Back to home</a>
 	</div>
