20cf63b098b6920bc44ce3a8d0b0da3ca1a9cb16
William GOURLEY <wgourley@e4r6p4.wethinkcode.co.za>
2018-10-07 13:04:29 +0200

    Filter

--- index.php
+++ index.php
@@ -2,12 +2,66 @@
 	<?php
 	include "head.php";	
 	?>
+	<div class="filter">
+		<h2>Filter</h2>
+		<hr>
+		<form class="filer_items">
+			<div>
+				<label for="catagory">Catagoty</label>
+				<select class="filt" name="Catagory">
+					<option value="4">All</option>
+					<option value="0">Mundane</option>
+					<option value="1">Special</option>
+					<option value="2">Exceptional</option>
+					<option value="3">Elderich</option>
+				</select>
+			</div>
+			<div>
+				<label for="min-price">Starting Price</label>
+				<input class="filt" type="number" name="min-price">
+			</div>
+			<div>
+				<label for="max-price">Maximum Price</label>
+				<input class="filt" type="number" name="max-price">
+			</div>
+			<input class="filt" type="submit" value="Filter">
+		</form>
+	</div>
 	<div class="product-list">
 	<?php
-	if(isset($_GET['filter']))
-		$products = send_query_arr(["select"=>"*", "from"=>"products", "where"=>"Catagory='".$_GET['filter']."'"]);
+
+
+	$catagories = [
+		"Mundane",
+		"Special",
+		"Exceptional",
+		"Elderich"
+	];
+
+	$Keys = [
+		"Catagory",
+		"min-price",
+		"max-price"
+	];
+
+	$Query = [];
+
+	foreach($Keys as $v){
+		if (isset($_GET[$v])&& is_numeric($_GET[$v])){
+			if ($v=="min-price")
+				array_push($Query, "Price>='$_GET[$v]'");
+			if ($v=="max-price")
+				array_push($Query, "Price<='$_GET[$v]'");
+			if ($v=="Catagory" && $_GET[$v] <= 3)
+				array_push($Query, "Catagory='".$catagories[$_GET[$v]]."'");
+		}
+	}
+	$Query = implode(" AND ", $Query);
+	if (!empty($Query))
+		$products = send_query_arr(["select"=>"*", "from"=>"products", "where"=>$Query]);
 	else
 		$products = send_query_arr(["select"=>"*", "from"=>"products"]);
+
 	foreach($products as $p){
 		?>
 		<div class="product" id="p<?php echo $p["id"] ?>">
